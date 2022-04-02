c4c940a3ecadc1c80267381bea1ca32753b998ca
ciatph <ciat.ph@gmail.com>
2022-04-02 23:36:09 +0800

    Testing gh secrets

--- src/run-me.js
+++ src/run-me.js
@@ -1,6 +1,6 @@
 const runMe = () => {
   console.log('Running script!')
-  console.log(`WEBSITE_URL is: "${process.env.WEBSITE_URL}"`)
+  console.log(`WEBSITE_URL is: "${process.env.WEBSITE_URL}", isGoogle? ${(process.env.WEBSITE_URL === 'https://google.com')}`)
   console.log('End of script run.')
 }
 
